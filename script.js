209d687baf08dcf3c494a4d82d8f643a9bc3a826
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2021-12-29 22:06:46 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//code
+let wrapper = document.querySelector(".wrapper"),
+  container = document.querySelector(".container"),
+  topNavParent = container.querySelector(".topNavParent"),
+  notesList = container.querySelector(".notesList"),
+  addNote = container.querySelector(".addNoteParent .addNote");
+
+
+  //navigation close?open
+
+  const
