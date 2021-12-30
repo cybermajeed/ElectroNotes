d1a284b4ea1d710bf3dda878ae2ee58c1b777600
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2021-12-30 09:19:39 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ let wrapper = document.querySelector(".wrapper"),
 
 //navigation close?open
 
-const navOpen = `<svg class="navOpen" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#ffffff">
+const navOpen = `<svg class="navOpen" xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 0 24 24" width="35px" fill="#ffffff">
   <path d="M0 0h24v24H0V0z" fill="none"/>
   <path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"/>
 </svg>`,
   //
-  navClose = `<svg class="navClose" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#fff">
+  navClose = `<svg class="navClose" xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 0 24 24" width="35px" fill="#fff">
   <path d="M0 0h24v24H0V0z" fill="none"/>
   <path d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"/>
 </svg>`;
 
 //function events
-topNavParent.querySelector("strong").onclick = navCloseOpen;
+topNavParent.querySelector("div").onclick = navCloseOpen;
 
 //
-topNavParent.querySelector("strong").innerHTML = navClose;
+topNavParent.querySelector("div").innerHTML = navClose;
 
 function navCloseOpen() {
-  if (topNavParent.querySelector("strong svg").classList.contains("navClose")) {
-    topNavParent.querySelector("strong").innerHTML = navOpen;
+  if (topNavParent.querySelector("div svg").classList.contains("navClose")) {
+    topNavParent.querySelector("div").innerHTML = navOpen;
   } else {
-    topNavParent.querySelector("strong").innerHTML = navClose;
+    topNavParent.querySelector("div").innerHTML = navClose;
   }
 }
