9455b6d3a3baafa5fa671b4d037b92711e3cef2c
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2021-12-31 14:16:57 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -71,15 +71,31 @@ addNote.onclick = createNewNote;
 function createNewNote(note) {
   let noteparentDiv = document.createElement("div");
   noteparentDiv.classList.add("notes");
+  let parentTop = document.createElement("div");
+  parentTop.classList.add("parentTop");
+
   let inputText = document.createElement("input");
   inputText.type = "text";
   inputText.classList.add("noteTitle");
   inputText.placeholder = "Title";
   inputText.maxLength = "30";
+
+  let deleteNote = document.createElement("button");
+  deleteNote.classList.add("deleteNote");
+  //
+  const deleteNoteIcon = `<svg class="deleteNoteIcon" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#000">
+<path d="M0 0h24v24H0V0z" fill="none"/>
+<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z"/>
+</svg>`;
+
+  deleteNote.innerHTML = deleteNoteIcon;
+  //
   let textarea = document.createElement("textarea");
   textarea.classList.add("noteContent");
   textarea.placeholder = "Type Here";
-  noteparentDiv.appendChild(inputText);
+  parentTop.appendChild(inputText);
+  parentTop.appendChild(deleteNote);
+  noteparentDiv.appendChild(parentTop);
   noteparentDiv.appendChild(textarea);
   notesList.appendChild(noteparentDiv);
   //local Storage
@@ -109,11 +125,3 @@ function updateLocalStorage() {
 setInterval(() => {
   //updateLocalStorage();
 }, 2000);
-
-//
-const deleteNoteIcon = `<svg class="deleteNoteIcon" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#000">
-<path d="M0 0h24v24H0V0z" fill="none"/>
-<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z"/>
-</svg>`;
-
-document.querySelector(".deleteNote").innerHTML = deleteNoteIcon;
