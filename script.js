72983dc253f3334b6dbe1fcb6391d9185b4ff811
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2022-01-11 07:38:58 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let wrapper = document.querySelector(".wrapper"),
   noteTitleInEditView = wrapper.querySelector(" input.noteTitleInEditView"),
   textareaInEditView = wrapper.querySelector("textarea.noteContentInEditView"),
   deleteNoteInEditView = wrapper.querySelector(" button.deleteNote"),
+  noteColorInEditView = wrapper.querySelector(" button.noteColor"),
   container = document.querySelector(".container"),
   topNavParent = container.querySelector(".topNavParent"),
   notesList = container.querySelector(".notesList"),
@@ -19,7 +20,12 @@ const deleteNoteIcon = `<svg class="deleteNoteIcon" xmlns="http://www.w3.org/200
 <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z"/>
 </svg>`;
 deleteNoteInEditView.innerHTML = deleteNoteIcon;
-
+//palette
+const noteColorIcon = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="30px" viewBox="0 0 24 24" width="30px" fill="#000000">
+<g><rect fill="none" height="24" width="24"/></g><g>
+<path d="M12,2C6.49,2,2,6.49,2,12s4.49,10,10,10c1.38,0,2.5-1.12,2.5-2.5c0-0.61-0.23-1.2-0.64-1.67c-0.08-0.1-0.13-0.21-0.13-0.33 c0-0.28,0.22-0.5,0.5-0.5H16c3.31,0,6-2.69,6-6C22,6.04,17.51,2,12,2z M17.5,13c-0.83,0-1.5-0.67-1.5-1.5c0-0.83,0.67-1.5,1.5-1.5 s1.5,0.67,1.5,1.5C19,12.33,18.33,13,17.5,13z M14.5,9C13.67,9,13,8.33,13,7.5C13,6.67,13.67,6,14.5,6S16,6.67,16,7.5 C16,8.33,15.33,9,14.5,9z M5,11.5C5,10.67,5.67,10,6.5,10S8,10.67,8,11.5C8,12.33,7.33,13,6.5,13S5,12.33,5,11.5z M11,7.5 C11,8.33,10.33,9,9.5,9S8,8.33,8,7.5C8,6.67,8.67,6,9.5,6S11,6.67,11,7.5z"/></g>
+</svg>`;
+noteColorInEditView.innerHTML = noteColorIcon;
 //
 const addNoteIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#FFF">
 <path d="M0 0h24v24H0V0z" fill="none"/>
@@ -228,11 +234,13 @@ setInterval(() => {
     noteTitleInEditView.disabled = true;
     textareaInEditView.disabled = true;
     deleteNoteInEditView.disabled = true;
+    noteColorInEditView.disabled = true;
     document.title = `Cyber Note`;
   } else {
     noteTitleInEditView.disabled = false;
     textareaInEditView.disabled = false;
     deleteNoteInEditView.disabled = false;
+    noteColorInEditView.disabled = false;
     if (noteTitleInEditView.value == "") {
       let generatedNoteTitle =
         textareaInEditView.value == ""
