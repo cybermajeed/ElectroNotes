4e4b1fa0876dc56a22925177e2a35cdcdc40c5f6
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2023-07-18 10:57:10 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -282,23 +282,27 @@ function createNewNote(note) {
 
 //word count
 function countWord() {
+
   let wordsArrayTemp = document
     .querySelector(".noteContentInEditView.editAreaDisabled")
     .innerText.replaceAll("\n", " ")
     .trim()
     .split(" ");
   wordsArray = [];
+  charCount = 0;
   for (let i of wordsArrayTemp) {
     if (i != "") {
+      charCount += i.length;
       wordsArray.push(i);
     }
   }
   let wCountWrapper = document.querySelector(
     ".wrapper.containsNote .wordCount"
   );
-  let Ccount = wordsArray.length;
+  let Ccount = charCount;
   let Wcount = Ccount == 0 ? 0 : wordsArray.length;
   wCountWrapper.innerHTML = `${Wcount} W; ${Ccount} C`;
+
 }
 //word count ends
 //modifienOn
