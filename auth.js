ed12cf149cb2bd286276017123fc300e1b6435bf
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2024-04-05 15:09:42 +0400

    V24.4.5
    added login function and profile pic

--- auth.js
+++ auth.js
@@ -5,6 +5,11 @@ import {
   onAuthStateChanged,
   signOut,
 } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import {
+  getStorage,
+  ref,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCLfrQVLdX98J_Axa1PEQ9h4pLUFHb32g",
@@ -24,6 +29,8 @@ password: "cyberMajeed333"
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const storage = getStorage(app, "gs://electronotes-fbf7e.appspot.com");
+
 //app version
 const version = "V24.4.4";
 let versionSpan = topNavParent.querySelector("div.profile .version");
@@ -31,16 +38,14 @@ let loginBtn = document.querySelector(".profile .login #loginBtn"),
   logoutBtn = document.querySelector(".profile .logout #logoutBtn"),
   profilePic = document.querySelector("div.profile img"),
   loginScreen = document.querySelector("div.profile .login"),
-  logoutScreen = document.querySelector("div.profile .logout");
+  logoutScreen = document.querySelector("div.profile .logout"),
+  username = document.getElementById("username"),
+  password = document.getElementById("password");
 //login
 loginBtn.addEventListener("click", (e) => {
-  let username = document.getElementById("username").value;
-  username += "@en.com";
-  let password = document.getElementById("password").value;
-  //
-
-  signInWithEmailAndPassword(auth, username, password)
-    .then(() => {
+  //login
+  signInWithEmailAndPassword(auth, username.value + "@en.com", password.value)
+    .then((cred) => {
       alert("Logged In");
       loginScreen.querySelector("#password").value = "";
       loginScreen.querySelector("#username").value = "";
@@ -70,7 +75,18 @@ logoutBtn.addEventListener("click", (e) => {
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    profilePic.title = user.email.split("@")[0].toUpperCase();
+    let userName = user.email.split("@")[0].toUpperCase();
+    profilePic.title = userName;
+    //set profile pic
+    const imgRef = ref(storage, `profiles/${userName}.png`);
+    getDownloadURL(imgRef)
+      .then((url) => {
+        profilePic.src = url;
+      })
+      .catch(() => {
+        console.log("Profile not found");
+      });
+    //sync
     profilePic.oncontextmenu = (e) => {
       e.preventDefault();
       //sync()
@@ -89,6 +105,8 @@ onAuthStateChanged(auth, (user) => {
     };
   } else {
     //if use not signed in
+    //set profile pic
+    profilePic.src = "./assets/profile.svg";
     profilePic.onclick = () => {
       loginScreen.style.display = "flex";
       versionSpan.style.display = "block";
